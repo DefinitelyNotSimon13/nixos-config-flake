408d4a0deeea3f1b23793eb237610f3bee2c74a7
Simon <simon21.blum@gmail.com>
2024-10-13 19:24:05 +0200

    Gen: 127 - 13.10.2024 - 19:24:05

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -111,10 +111,9 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages =
-    with pkgs;
-    [
-    ];
+  environment.systemPackages = with pkgs; [
+    btop
+  ];
 
   services.pcscd.enable = true;
 
