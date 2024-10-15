73406b355d72b0b728dad9db117868912fc05171
Simon <simon21.blum@gmail.com>
2024-10-15 13:18:06 +0200

    Gen: 165 - 15.10.2024 - 13:18:06

--- pkgs/util-scripts/rebuild.nix
+++ pkgs/util-scripts/rebuild.nix
@@ -7,7 +7,7 @@ pkgs.writeShellScriptBin "rebuild" ''
   git add .
   git diff -U0
 
-  ${pkgs.nh}/bin/nh os switch || (echo "An error occurred! Aborting..."; exit)
+  ${pkgs.nh}/bin/nh os switch || (echo "An error occurred! Aborting..." && false)
 
   generation=$(nixos-rebuild list-generations | grep current | awk '{print $1}')
   date=$(date '+%d.%m.%Y - %H:%M:%S')
