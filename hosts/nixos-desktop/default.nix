8d6faa1551c0698bbc3da7f851c9b59a5b697bce
Simon <simon21.blum@gmail.com>
2024-11-20 11:32:30 +0100

    Gen: 251 - 20.11.2024 - 11:32:30

--- hosts/nixos-desktop/default.nix
+++ hosts/nixos-desktop/default.nix
@@ -7,4 +7,16 @@
   sops.age.keyFile = "/home/simon/.config/sops/age/keys.txt";
   minecraftServer.enable = true;
   gaming.enable = true;
+
+  fileSystems."/home/simon/vwi" = {
+    device = "ud04_266@ud04.udmedia.de:/home/ud04_266";
+    fsType = "sshfs";
+    options = [
+      "nodev"
+      "noatime"
+      "allow_other"
+      "IdentityFile=/home/simon/.ssh/id_ed25519"
+      "Port=2222"
+    ];
+  };
 }
