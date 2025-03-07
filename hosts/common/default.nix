b5d14ffb2716162afc947565375d10be93385445
Simon <simon21.blum@gmail.com>
2025-03-07 11:46:57 +0100

    Gen: 116 - 07.03.2025 - 11:46:57

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -17,8 +17,8 @@
 
   services.nginx = {
     enable = true;
-    recommendedProxySettings = true;
-    recommendedTlsSettings = true;
+    recommendedProxySettings = false;
+    recommendedTlsSettings = false;
     virtualHosts."localhost" = {
       listen = [
         {
