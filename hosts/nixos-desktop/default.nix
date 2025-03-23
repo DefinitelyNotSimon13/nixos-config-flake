ce78b505bb57b498dcf714235097ee62fc74880d
Simon <simon21.blum@gmail.com>
2025-03-23 15:52:49 +0100

    Gen: 111 - 23.03.2025 - 15:52:49

--- hosts/nixos-desktop/default.nix
+++ hosts/nixos-desktop/default.nix
@@ -8,10 +8,17 @@
   minecraftServer.enable = false;
   gaming.enable = false;
 
-  virtualisation.oci-containers.containers = {
-    plantuml = {
-      image = "plantuml/plantuml-server:jetty";
-      ports = [ "8091:8080" ];
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers = {
+      plantuml = {
+        image = "plantuml/plantuml-server:jetty";
+        ports = [ "8091:8080" ];
+      };
+      dividi = {
+        image = "definitelynotsimon13/dividi";
+        ports = [ "8005:8000" ];
+      };
     };
   };
 }
