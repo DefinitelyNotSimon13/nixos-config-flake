5d8753e0f87224785f57ac8d74332781d8eff93a
Simon Blum <simon21.blum@gmail.com>
2025-12-08 13:08:21 +0100

    Gen:  - 08.12.2025 - 13:08:21

--- home/features/desktop/hypr/hyprland.nix
+++ home/features/desktop/hypr/hyprland.nix
@@ -161,6 +161,8 @@ in
           ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
           ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
           ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          ", XF86MonBrightnessUp, exec, brightnessctl s +5%"
+          ", XF86MonBrightnessDown, exec, brightnessctl s 5%-"
 
         ];
 
