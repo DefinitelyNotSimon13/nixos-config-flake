e45e4b4b1e0f1b3a0ac0298273a97b282c004bd7
Simon Blum <simon21.blum@gmail.com>
2025-12-08 14:32:39 +0100

    Gen:  - 08.12.2025 - 14:32:39

--- home/features/desktop/hypr/hyprland.nix
+++ home/features/desktop/hypr/hyprland.nix
@@ -69,8 +69,8 @@ in
 
         general = {
           gaps_in = 0;
-          gaps_out = 0;
-          border_size = 0;
+          gaps_out = 1;
+          border_size = 1;
           "col.active_border" = lib.mkForce "rgba(fab387ff)";
           "col.inactive_border" = lib.mkForce "rgba(595959ff)";
           layout = "dwindle";
